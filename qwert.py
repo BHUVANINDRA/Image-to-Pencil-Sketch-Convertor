8ef86a590c58bce10c3101710fbed6d5b1e65e47
BHUVANINDRA <123956630+BHUVANINDRA@users.noreply.github.com>
2025-02-10 21:43:41 +0530

    Add files via upload

---
+++ qwert.py
@@ -0,0 +1,39 @@
+import cv2
+import matplotlib
+import matplotlib.pyplot as plt
+
+img = cv2.imread("image2.png")
+img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+plt.figure(figsize=(8,8))
+plt.imshow(img)
+plt.axis("off")
+plt.title("Original Image")
+plt.show()
+
+img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+plt.figure(figsize=(8,8))
+plt.imshow(img_gray,cmap="gray")
+plt.axis("off")
+plt.title("Grayscale Image")
+plt.show()
+
+img_invert = cv2.bitwise_not(img_gray)
+plt.figure(figsize=(8,8))
+plt.imshow(img_invert,cmap="gray")
+plt.axis("off")
+plt.title("Invert Image")
+plt.show()
+
+img_smoothing = cv2.bitwise_not(img_invert, (21,21),sigmaX=0, sigmaY=0)
+plt.figure(figsize=(8,8))
+plt.imshow(img_smoothing,cmap="gray")
+plt.axis("off")
+plt.title("Smoothen Image")
+plt.show()
+
+final = cv2.divide(img_gray, 255 - img_smoothing, scale=255)
+plt.figure(figsize=(8,8))
+plt.imshow(final,cmap="gray")
+plt.axis("off")
+plt.title("Final Sketch Image")
+plt.subplot(1,1,1)
